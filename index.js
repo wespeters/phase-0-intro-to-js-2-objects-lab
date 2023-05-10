fde69598a95485b4c38fc5fa1f109180c847c75f
Wesley Peters <wesleypeters@gmail.com>
2023-05-09 20:59:40 -0600

    modified objects

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Lewis Hamilton",
+    streetAdress: "1500 Grand Prix Drive",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+const newerEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Max Verstappen"
+    );
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+const newestEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Max Verstappen");
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    newEmployee[key] = undefined
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined;
+    return employee;
+};
